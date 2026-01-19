e90a3578ee76d4f1b3fa07ddf1e959cf6e5b8a98
Azzam Fajro <muhammadazzamfajro@gmail.com>
2026-01-19 08:43:29 +0700

    fix: add autoload regeneration and migration to deployment config to fix Logbook class not found error

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -9,4 +9,14 @@ class Kelas extends Model
 {
     /** @use HasFactory<\Database\Factories\KelasFactory> */
     use HasFactory;
+
+    protected $fillable = ['nama'];
+
+    /**
+     * Get all siswas in this kelas.
+     */
+    public function siswas()
+    {
+        return $this->hasMany(Siswa::class);
+    }
 }
